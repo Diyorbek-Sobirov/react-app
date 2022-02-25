d0ca7493956d94eacc451e40ec3dba942b50779f
kamalov <kfarrux2005@gmail.com>
2022-02-25 15:48:32 +0500

    Kamalov Added Counter

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.scss';
 import TodoList from './Components/TodoList/TodoList';
 import Modal from './Components/Modal/Modal';
 import MJurayev from './Components/MJurayev/MJurayev';
+import Kamalov from './Components/Kamalov/counter';
 function App() {
   const [open, setOpen] = useState(false)
   const handleClick = () => {
@@ -14,6 +15,7 @@ function App() {
       <Modal setIsOpen={setOpen} isOpen={open} />
       <TodoList />
       <MJurayev />
+      <Kamalov /> 
     </div>
   );
 }
---
+++ src/Components/Kamalov/counter.jsx
@@ -0,0 +1,26 @@
+import react from 'react';
+
+
+function Counter(){
+    const [counter, setCounter] = useState(0);
+    function add(){
+        setCounter(counter + 1);
+    }
+    function minus(){
+        setCounter(counter - 1);
+    }
+    function restart(){
+        setCounter(0);
+    }
+    return(
+        <div>
+            <h1>{counter}</h1>
+            <button onClick={add}>+</button>
+            <button onClick={minus}>-</button>
+            <button onClick={restart}>restart</button>
+        </div>
+    )
+}
+
+
+export default Counter;
