b1fcaaa226849f102c5d2149956bf4dd147b85bb
MJurayev <jurayevmansurbek667@gmail.com>
2022-02-25 15:24:07 +0500

    added myself component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import './App.scss';
 import TodoList from './Components/TodoList/TodoList';
 import Modal from './Components/Modal/Modal';
+import MJurayev from './Components/MJurayev/MJurayev';
 function App() {
   const [open, setOpen] = useState(false)
   const handleClick = () => {
@@ -12,8 +13,7 @@ function App() {
       <button onClick={handleClick} >Open Modal</button>
       <Modal setIsOpen={setOpen} isOpen={open} />
       <TodoList />
-
-      
+      <MJurayev />
     </div>
   );
 }
